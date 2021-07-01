9b7fc87483975ab0e3836785a05b82f4691cf2a9
ChanhoYoon <chyoon0512@gmail.com>
2021-07-01 19:12:08 +0900

    Add : 깃허브 검색 리스트 레이아웃 작성 ul 태그 map을 사용해서 ListItem 컴포넌트로 전달하여 각각 다른 정보를 출력

--- src/components/List/apiList.tsx
+++ src/components/List/apiList.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import styled from "styled-components";
+import useGetUsers from "../../hooks/users/useGetUsers";
+import ListItem from "./listItem";
 
 const ApiList = () => {
+   const users = useGetUsers();
    return (
-         <h1>여기다</h1>
+      <Wrapper>
+         <ul>
+            {users?.map((item) => (
+               <ListItem key={item.id} user={item} />
+            ))}
+         </ul>
+      </Wrapper>
    );
 };
 
 export default ApiList
+
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`
