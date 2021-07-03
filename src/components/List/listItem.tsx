8beb0732c2a69cc9ba87d3faf205bcc137fcdbd3
ChanhoYoon <chyoon0512@gmail.com>
2021-07-03 17:01:57 +0900

    Modify : 버그가 있던 로직 수정 ( 추가,삭제시에 버튼을 두 번 눌러야 됐던 문제 해결)

--- src/components/List/listItem.tsx
+++ src/components/List/listItem.tsx
@@ -1,89 +1,90 @@
-import React, {useState} from 'react';
-import {UserObj} from "../../modules/users";
-import styled from "styled-components";
-import {FaGithubAlt} from 'react-icons/fa';
-import useGetLocalDatas from "../../hooks/localstorage/useGetLocalDatas";
-import useSetLocalDatas from "../../hooks/localstorage/useSetLocalDatas";
-import useRemoveLocalDatas from "../../hooks/localstorage/useRemoveLocalDatas";
-import useGetIndexLocalData from "../../hooks/localstorage/useGetIndexLocalData";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { UserObj } from '../../modules/users';
+import { FaGithubAlt } from 'react-icons/fa';
+import useSetLocalDatas from '../../hooks/localstorage/useSetLocalDatas';
+import useRemoveLocalDatas from '../../hooks/localstorage/useRemoveLocalDatas';
+import useGetIndexLocalData from '../../hooks/localstorage/useGetIndexLocalData';
 
 const ListItem = (item: UserObj) => {
-   const [toggle, setToggle] = useState(false);
-   const getLocalStorageItems = useGetLocalDatas();
-   const setLocalStorageItems = useSetLocalDatas();
-   const removeLocalStorageItems = useRemoveLocalDatas();
-   const getIndexLocalData = useGetIndexLocalData();
+  const [, setToggle] = useState(false);
+  const setLocalStorageItems = useSetLocalDatas();
+  const removeLocalStorageItems = useRemoveLocalDatas();
+  const getIndexLocalData = useGetIndexLocalData();
 
-   const onClick = () => {
-      const localStorageItem = getLocalStorageItems();
-
-      if (!toggle) {
-         setLocalStorageItems({
-            id: item.user.id,
-            login: item.user.login,
-            avatar_url: item.user.avatar_url,
-            html_url: item.user.html_url
-         })
-      } else {
-         if (localStorageItem !== undefined)
-            removeLocalStorageItems(item.user.id);
-      }
+  const onClick = () => {
+    if (getIndexLocalData(item.user.id)) {
+      removeLocalStorageItems(item.user.id);
       setToggle((toggle) => !toggle);
-   }
-   return (
-      <Li>
-         {item.user.isFirstWord !== '' && <span className='first_word'>{item.user.isFirstWord}</span>}
-         <img src={item.user.avatar_url} alt="profile_image" />
-         <span>{item.user.login}</span>
-         <button className={getIndexLocalData(item.user.id) ? 'active' : ''} onClick={onClick}>
-            <FaGithubAlt />
-         </button>
-      </Li>
-   )
-}
+    } else {
+      setLocalStorageItems({
+        id: item.user.id,
+        login: item.user.login,
+        avatar_url: item.user.avatar_url,
+        html_url: item.user.html_url,
+      });
+      setToggle((toggle) => !toggle);
+    }
+  };
+  return (
+    <Li>
+      {item.user.isFirstWord !== '' && (
+        <span className="first_word">{item.user.isFirstWord}</span>
+      )}
+      <img src={item.user.avatar_url} alt="profile_image" />
+      <span>{item.user.login}</span>
+      <button
+        className={getIndexLocalData(item.user.id) ? 'active' : ''}
+        onClick={onClick}
+      >
+        <FaGithubAlt />
+      </button>
+    </Li>
+  );
+};
 
 export default ListItem;
 
 const Li = styled.li`
-    position: relative;
-    width: 100%;
-    height: 50px;
+  position: relative;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 0 20px 0;
+
+  .first_word {
+    position: absolute;
+    top: 15px;
+    left: -90px;
+    width: 10px;
+    padding: 0 20px 0 0;
+    border-right: 5px solid #00c400;
+    font-size: 1.5rem;
+  }
+
+  img {
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
+  }
+
+  span {
+    width: 350px;
+    padding-left: 40px;
+    font-size: 1.5rem;
+  }
+
+  button {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    margin: 0 0 20px 0;
+    font-size: 4rem;
+    cursor: pointer;
+    color: lightgray;
 
-    .first_word {
-        position: absolute;
-        top: 15px;
-        left: -90px;
-        width: 10px;
-        padding: 0 20px 0 0;
-        border-right: 5px solid #00c400;
-        font-size: 1.5rem;
+    &.active {
+      color: #00c400;
     }
-
-    img {
-        width: 45px;
-        height: 45px;
-        border-radius: 50%;
-    }
-
-    span {
-        width: 350px;
-        padding-left: 40px;
-        font-size: 1.5rem;
-    }
-
-    button {
-        display: flex;
-        align-items: center;
-        font-size: 4rem;
-        cursor: pointer;
-        color: lightgray;
-
-        &.active {
-            color: #00c400;
-        }
-    }
-`
+  }
+`;
