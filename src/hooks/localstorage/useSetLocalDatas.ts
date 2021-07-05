a59694b0f74c774a1d309657f1e4c49a18c2d036
ChanhoYoon <chyoon0512@gmail.com>
2021-07-05 18:05:27 +0900

    Add : 즐겨찾기 추가시 favorites state(상태)에 해당 아이템을 추가

--- src/hooks/localstorage/useSetLocalDatas.ts
+++ src/hooks/localstorage/useSetLocalDatas.ts
@@ -1,36 +1,46 @@
-import { FavoriteUser } from '../../modules/users';
 import { useCallback } from 'react';
 import useGetLocalDatas from './useGetLocalDatas';
+import { addFavorites, Favorites } from '../../modules/favorites';
+import { useDispatch } from 'react-redux';
 
 const LOCAL_KEY = 'favorite';
 
 export default function useSetLocalDatas() {
   const getLocalStorageItems = useGetLocalDatas();
+  const dispatch = useDispatch();
   return useCallback(
-    (item: FavoriteUser) => {
+    (item: Favorites) => {
+      const newItem = {
+        id: item.id,
+        login: item.login,
+        avatar_url: item.avatar_url,
+        html_url: item.html_url,
+        events_url: item.events_url,
+        followers_url: item.followers_url,
+        following_url: item.following_url,
+        gists_url: item.gists_url,
+        gravatar_id: item.gravatar_id,
+        node_id: item.node_id,
+        organizations_url: item.organizations_url,
+        received_events_url: item.received_events_url,
+        repos_url: item.repos_url,
+        score: item.score,
+        site_admin: item.site_admin,
+        starred_url: item.starred_url,
+        subscriptions_url: item.subscriptions_url,
+        type: item.type,
+        url: item.url,
+      };
       const localStorageItem = getLocalStorageItems();
+
       if (localStorageItem === undefined) {
-        localStorage.setItem(
-          LOCAL_KEY,
-          JSON.stringify([
-            {
-              id: item.id,
-              login: item.login,
-              avatar_url: item.avatar_url,
-              html_url: item.html_url,
-            },
-          ]),
-        );
+        localStorage.setItem(LOCAL_KEY, JSON.stringify([newItem]));
       } else {
         const newFavoriteUser = localStorageItem;
-        newFavoriteUser.push({
-          id: item.id,
-          login: item.login,
-          avatar_url: item.avatar_url,
-          html_url: item.html_url,
-        });
+        newFavoriteUser.push(newItem);
         localStorage.setItem(LOCAL_KEY, JSON.stringify(newFavoriteUser));
       }
+      dispatch(addFavorites(newItem));
     },
     [getLocalStorageItems],
   );
