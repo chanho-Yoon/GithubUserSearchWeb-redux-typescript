b9701afec989325a0177978aeaa7188d88a9cccc
ChanhoYoon <chyoon0512@gmail.com>
2021-07-05 18:04:01 +0900

    Add : localStorage에 있는 즐겨찾기 아이템을 가져오는데 if문 실행이 안됐을 시 return문을 안줬더니 `undefined` 반환으로 인한 버그 발생 else 문에 return [] 빈배열 추가

--- src/hooks/localstorage/useGetLocalDatas.ts
+++ src/hooks/localstorage/useGetLocalDatas.ts
@@ -5,8 +5,8 @@ const LOCAL_KEY = 'favorite';
 export default function useGetLocalDatas() {
   return useCallback(() => {
     const getLocalStorageItem = localStorage.getItem(LOCAL_KEY);
-    if (getLocalStorageItem != null) {
+    if (getLocalStorageItem !== null) {
       return JSON.parse(getLocalStorageItem);
-    }
+    } else return [];
   }, []);
 }
