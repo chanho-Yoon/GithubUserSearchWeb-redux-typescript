6e858e008e13f918f99001c4ee3838836c26bb06
ChanhoYoon <chyoon0512@gmail.com>
2021-07-05 18:01:50 +0900

    Add : useGetFavorites 커스텀 훅을 사용해 가져온 state를 LocalListItem으로 전달

--- src/components/List/localList.tsx
+++ src/components/List/localList.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Favorites } from '../../modules/favorites';
+import useGetFavorites from '../../hooks/favorites/useGetFavorites';
+import LocalListItem from './localListItem';
 
 const LocalList = () => {
+  const getFavorites = useGetFavorites();
   return (
-    <div>
-      <span>LocalList</span>
-    </div>
+    <Wrapper>
+      <ul>
+        {getFavorites?.map((item: Favorites) => (
+          <LocalListItem key={item.id} user={item} />
+        ))}
+      </ul>
+    </Wrapper>
   );
 };
 
 export default LocalList;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
